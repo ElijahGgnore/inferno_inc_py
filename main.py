8965e3ceeb7f809af78f7c7bdc03122926e829fb
ElijahGgnore <elijahg.gnore@gmail.com>
2025-01-22 21:37:32 +1000

    Add text message input and preliminary callbacks

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from message_log import MessageLog, TextMessage, TextMessagePart
 mp1 = TextMessagePart('Welcome', symbol_delay=1 / 10)
 mp2 = TextMessagePart('\nNo time to explain', symbol_delay=1 / 100, auto_continue=True)
 mp3 = TextMessagePart('\nThis one doesn\'t get typed', symbol_delay=0)
+mp4 = TextMessagePart("\nWhat's your name?\n", store_input_at='name')
 
-tm = TextMessage([mp1, mp2, mp3])
+
+def greeting(t: TextMessage, p: TextMessagePart):
+    p.text = '\nHello ' + t.input_dict['name'] + '\n'
+
+
+mp5 = TextMessagePart('', preliminary_callback=greeting)
+
+tm = TextMessage([mp1, mp2, mp3, mp4, mp5])
 
 game = stage.Stage()
 scene = MessageLog()
--- message_log.py
+++ message_log.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from collections import deque
 from collections.abc import Callable
 from typing import Any
@@ -18,6 +19,7 @@ class MessageLog(stage.Scene):
 
     def append_message(self, message: Message):
         self.list_body.append(message)
+        self.list_box.focus_position = len(self.list_body) - 1
         message.setup(self)
 
 
@@ -45,33 +47,70 @@ class TextMessage(Message):
 
         self._continue_hint = urwid.Text('[Press ENTER to continue]')
         self._can_continue = False
-        self._auto_continue = False
+
+        self.input_dict: dict[str, str] = {}
+        self._current_part: TextMessagePart | None = None
 
         super().__init__(self._pile)
 
+    @property
+    def typewriter(self):
+        return self._typewriter
+
+    @property
+    def pile(self):
+        return self._pile
+
     def setup(self, message_log):
         super().setup(message_log)
-        urwid.connect_signal(self._typewriter, 'symbol_typed',
+        urwid.connect_signal(self._typewriter,
+                             'symbol_typed',
                              signal_callback_stub(message_log.stage.urwid_loop.draw_screen))
-        urwid.connect_signal(self._typewriter, 'finished_typing', signal_callback_stub(
-            lambda: self.continue_() if self._auto_continue else self.enable_continue()))
+
+        def on_typewriter_finished():
+            if self._current_part is not None:
+                if self._current_part.store_input_at is not None:
+                    self._typewriter.enable_input()
+                    self._message_log.stage.urwid_loop.draw_screen()
+                elif self._current_part.auto_continue:
+                    self.continue_()
+                else:
+                    self.enable_continue()
+
+        urwid.connect_signal(self._typewriter,
+                             'finished_typing',
+                             signal_callback_stub(on_typewriter_finished))
         self.continue_()
 
     def keypress(self, size: tuple[()] | tuple[int] | tuple[int, int], key: str) -> str | None:
-        if key == 'enter' and self._can_continue:
-            self.disable_continue()
-            self.continue_()
-            return None
+        if key == 'enter':
+            if self._current_part is not None:
+                if self._current_part.store_input_at is not None:
+                    self.input_dict[self._current_part.store_input_at] = self._typewriter.edit_widget.edit_text
+                    self._typewriter.disable_input()
+                    self.continue_()
+                    return None
+                elif self._can_continue:
+                    self.disable_continue()
+                    self.continue_()
+                    return None
+
         return super().keypress(size, key)
 
     def continue_(self):
         if len(self.parts):
-            m = self.parts.popleft()
-            self._auto_continue = m.auto_continue
+            self._current_part = self.parts.popleft()
+
+            if self._current_part.preliminary_callback:
+                self._current_part.preliminary_callback(self, self._current_part)
+
             self._message_log.stage.asyncio_loop.create_task(
-                self._typewriter.type(m.text, symbol_delay=m.symbol_delay, append_text=m.append_text))
+                self._typewriter.type(self._current_part.text,
+                                      symbol_delay=self._current_part.symbol_delay,
+                                      append_text=self._current_part.append_text))
         else:
-            if self.final_callback:
+            self._current_part = None
+            if self.final_callback is not None:
                 self.final_callback(self)
 
     def enable_continue(self):
@@ -89,8 +128,12 @@ class TextMessage(Message):
 
 class TextMessagePart:
     def __init__(self, text: str, append_text: bool = True, symbol_delay: float | None = None,
-                 auto_continue: bool = False):
+                 auto_continue: bool = False, store_input_at: str | None = None, append_input: bool = True,
+                 preliminary_callback: Callable[[TextMessage, TextMessagePart], Any] | None = None):
         self.text = text
         self.append_text = append_text
         self.symbol_delay = symbol_delay
         self.auto_continue = auto_continue
+        self.store_input_at = store_input_at
+        self.append_input = append_input
+        self.preliminary_callback = preliminary_callback
